2beebe363692fe0cd73039e166536fc264017031
RayHsiao <kevin1002045@gmail.com>
2024-04-11 21:25:47 +0800

    Initial Commit

---
+++ Just ALL IN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Just ALL IN
+//
+//  Created by 蕭煜勳 on 2024/4/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
